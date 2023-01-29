2f689330f73479cf6ae67489fd964cafd60c6b8d
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-01-29 20:54:37 +0100

    Install Preline UI

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import "preline"
 
 const app = createApp(App)
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+        "node_modules/preline/dist/*.js",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('preline/plugin'),
+    ],
 }
