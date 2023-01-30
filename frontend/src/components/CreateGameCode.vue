ef14f0e7474033950ebacbda0128f3104c655b3f
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-01-30 22:30:17 +0100

    Create CreateNewGame.vue + CreateGameCode.vue

---
+++ frontend/src/components/CreateGameCode.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+      class="flex flex-col w-2/5 h-80 bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+    <div class="bg-gray-100 border-b rounded-t-xl py-3 px-4 md:py-4 md:px-5 dark:bg-gray-800 dark:border-gray-700">
+      <p class="mt-1 text-sm text-gray-500 dark:text-gray-500">
+        Player 1
+      </p>
+    </div>
+    <div class="flex grow flex-col p-4 md:p-5">
+      <h3 class="flex-none text-lg font-bold text-gray-800 dark:text-white">
+        Card title
+      </h3>
+      <p class="grow flex mt-2 text-gray-800 dark:text-gray-400 text-6xl font-mono h-fill items-center content-center justify-center">
+        ----
+      </p>
+      <a class="flex-none  mt-3 mx-auto w-full inline-flex items-center gap-2 mt-5 text-sm font-medium text-blue-500 hover:text-blue-700"
+         href="#">
+        <button type="button"
+                class="py-[.688rem] px-4 mx-auto w-full inline-flex justify-center items-center gap-2 rounded-md border-2 border-gray-200 font-semibold text-blue-500 hover:text-white hover:bg-blue-500 hover:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:border-gray-700 dark:hover:border-blue-500">
+          Create Game Code
+        </button>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/CreateNewGame.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex max-w-4xl content-center self-center items-center justify-center mx-auto space-x-8 h-screen">
+    <CreateGameCode/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CreateGameCode from "@/components/CreateGameCode.vue";
+</script>
+
+<style scoped>
+
+</style>
