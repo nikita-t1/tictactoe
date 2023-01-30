93ef0aee3b5d80ed744351228513318f82f0487c
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-01-30 22:31:16 +0100

    Add Routes to Vue-Router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,17 +5,27 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/start',
+      name: 'start',
+      component: () => import('../views/StartView.vue')
+    },
+    {
+      path: '/create-new-game',
+      name: 'create-new-game',
+      component: () => import('../views/CreateNewGame.vue')
+    },
+    {
+      path: '/join-existing-game',
+      name: 'join-existing-game',
+      component: () => import('../views/JoinExistingGame.vue')
     }
   ]
 })
