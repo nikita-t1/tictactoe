695efa0ec7bb705e0992f0a0d2c7068cd203952e
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-02-01 19:11:36 +0100

    Add '/create-game-code' Route in Server

--- src/main/kotlin/dev/nikitatarasov/plugins/Routing.kt
+++ src/main/kotlin/dev/nikitatarasov/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package dev.nikitatarasov.plugins
 
+import dev.nikitatarasov.model.Game
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
@@ -8,6 +9,13 @@ fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+        post("/create-game-code") {
+            var gameCode: String
+            do {
+                gameCode = Game.randomGameId()
+            } while (Controller.games.any { it.id == gameCode })
+            call.respondText { gameCode }
         }
     }
 }
