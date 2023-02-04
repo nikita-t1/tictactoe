7249631f159558afdc09d96fc4d702e0c9b0087d
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-02-04 22:44:40 +0100

    Add config for Ktor executable JAR in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,12 @@ application {
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
+ktor {
+    fatJar {
+        archiveFileName.set("fat.jar")
+    }
+}
+
 repositories {
     mavenCentral()
 }
