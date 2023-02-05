3dcde588164d5a81ec773e058cd90ff89c845bd2
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-02-05 18:18:30 +0100

    Add 'singlePageApplication' Endpoint to Routing.kt
    Serve Vue.js from 'dist' folder

--- src/main/kotlin/dev/nikitatarasov/plugins/Routing.kt
+++ src/main/kotlin/dev/nikitatarasov/plugins/Routing.kt
@@ -1,21 +1,27 @@
 package dev.nikitatarasov.plugins
 
+import dev.nikitatarasov.Controller
 import dev.nikitatarasov.model.Game
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
         post("/create-game-code") {
             var gameCode: String
             do {
                 gameCode = Game.randomGameId()
-            } while (Controller.games.any { it.id == gameCode })
+            } while (Controller.findGame(gameCode) != null)
             call.respondText { gameCode }
+        }
+        get("/gameCodes") {
+            call.respondText { Controller.getAllGames().toString() }
+        }
+        singlePageApplication {
+            useResources = true
+            vue("dist")
         }
     }
 }
