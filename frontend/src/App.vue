510b8a5305ebf12700e069d283d897b00e028350
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-02-06 19:03:44 +0100

    Add Dark Mode Toggle

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import ThemeToggle from "@/components/ThemeToggle.vue";
 </script>
 
 <template>
-  <RouterView />
+  <div class="bg-gray-700">
+    <ThemeToggle />
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
