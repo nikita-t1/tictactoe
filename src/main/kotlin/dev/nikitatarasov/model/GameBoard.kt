fa480243d8068293d1203357a208cedcefb107bf
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-02-06 20:47:26 +0100

    Fix Error in Winner Validation

--- src/main/kotlin/dev/nikitatarasov/model/GameBoard.kt
+++ src/main/kotlin/dev/nikitatarasov/model/GameBoard.kt
@@ -43,12 +43,12 @@ class GameBoard {
             // | |x| |
             // | |x| |
             // | |x| |
-            else if ( gamefield[1][0].equalsAll(gamefield[1][0], gamefield[2][0]) ) winningSymbol = gamefield[1][0]
+            else if ( gamefield[0][1].equalsAll(gamefield[1][1], gamefield[2][1]) ) winningSymbol = gamefield[1][0]
 
             // | | |x|
             // | | |x|
             // | | |x|
-            else if ( gamefield[2][0].equalsAll(gamefield[1][0], gamefield[2][0]) ) winningSymbol = gamefield[2][0]
+            else if ( gamefield[0][2].equalsAll(gamefield[1][2], gamefield[2][2]) ) winningSymbol = gamefield[2][0]
 
             // |x| | |
             // | |x| |
