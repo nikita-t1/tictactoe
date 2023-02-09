c6688683496f8e114bec3ecb18e04ff5b71ae063
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-02-09 18:55:19 +0100

    Add proper Light/Dark Styling to CreateGameCode.vue

--- frontend/src/components/CreateGameCode.vue
+++ frontend/src/components/CreateGameCode.vue
@@ -1,22 +1,21 @@
 <template>
   <div>
-    <div
-        class="flex flex-col w-96 h-80 bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-      <div class="bg-gray-100 border-b rounded-t-xl py-3 px-4 md:py-4 md:px-5 dark:bg-gray-800 dark:border-gray-700">
-        <p class="mt-1 text-sm text-gray-500 dark:text-gray-500">
+    <div class="transition-all duration-700 flex flex-col w-96 h-80 bg-white border shadow-sm rounded-xl dark:bg-gray-800  dark:border-gray-700 dark:shadow-slate-700/[.7]">
+      <div class="transition-all duration-700 bg-gray-100 border-b rounded-t-xl py-3 px-4 md:py-4 md:px-5 dark:bg-gray-800 dark:border-gray-700">
+        <p class="transition-all duration-700 mt-1 text-sm text-gray-500 dark:text-gray-500">
           Player 1
         </p>
       </div>
       <div class="flex grow flex-col p-4 md:p-5">
-        <h3 class="flex-none text-lg font-bold text-gray-800 dark:text-white">
+        <h3 class="transition-all duration-700 flex-none text-lg font-bold text-gray-800 dark:text-white">
           Card title
         </h3>
-        <p class="grow flex mt-2 text-gray-800 dark:text-gray-400 text-6xl font-mono h-fill items-center content-center justify-center">
+        <p class="transition-all duration-700 grow flex mt-2 text-gray-500 dark:text-gray-400 text-6xl font-mono h-fill items-center content-center justify-center">
           {{ gameCode }}
         </p>
-        <a class="flex-none  mt-3 mx-auto w-full inline-flex items-center gap-2 mt-5 text-sm font-medium text-blue-500 hover:text-blue-700">
+        <a class="transition-all duration-700 flex-none mt-3 mx-auto w-full inline-flex items-center gap-2 mt-5 text-sm font-medium text-blue-500 hover:text-blue-700">
           <button type="button" @click="requestGameCode"
-                  class="py-[.688rem] px-4 mx-auto w-full inline-flex justify-center items-center gap-2 rounded-md border-2 border-gray-200 font-semibold text-blue-500 hover:text-white hover:bg-blue-500 hover:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:border-gray-700 dark:hover:border-blue-500">
+            class="py-[.688rem] px-4 mx-auto w-full inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">
             Create Game Code
           </button>
         </a>
@@ -24,13 +23,13 @@
     </div>
 
     <div :class="msg !== '' ? 'visible opacity-100' : 'invisible opacity-0'" class="transition-all duration-1000">
-      <div class="flex mt-4 border-2 border-blue-400 rounded-xl p-2 text-white bg-white shadow-sm dark:bg-gray-800 dark:shadow-slate-700/[.7]">
+      <div class="transition-all duration-700 flex mt-4 rounded-xl p-2 text-white bg-white border shadow-sm rounded-xl dark:bg-gray-800  dark:border-gray-700 dark:shadow-slate-700/[.7]">
         <div
             class="animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full"
             role="status" aria-label="loading">
           <span class="sr-only">Loading...</span>
         </div>
-        <span class="ml-4 content-start justify-center">
+        <span class="transition-all duration-700 ml-4 content-start justify-center text-gray-800 dark:text-white">
         {{ msg }}
       </span>
 
