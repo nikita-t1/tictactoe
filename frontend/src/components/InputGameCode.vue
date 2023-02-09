a627cc29ddd1ccd494654a03bcb6ec38bae29823
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-02-09 18:55:29 +0100

    Add proper Light/Dark Styling to InputGameCode.vue

--- frontend/src/components/InputGameCode.vue
+++ frontend/src/components/InputGameCode.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="flex flex-col w-96 h-80 bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-    <div class="bg-gray-100 border-b rounded-t-xl py-3 px-4 md:py-4 md:px-5 dark:bg-gray-800 dark:border-gray-700">
-      <p class="mt-1 text-sm text-gray-500 dark:text-gray-500">
+  <div class="transition-all duration-700 flex flex-col w-96 h-80 bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+    <div class="transition-all duration-700 bg-gray-100 border-b rounded-t-xl py-3 px-4 md:py-4 md:px-5 dark:bg-gray-800 dark:border-gray-700">
+      <p class="transition-all duration-700 mt-1 text-sm text-gray-500 dark:text-gray-500">
         Player 2
       </p>
     </div>
     <div class="flex grow flex-col p-4 md:p-5">
-      <h3 class="flex-none text-lg font-bold text-gray-800 dark:text-white">
+      <h3 class="transition-all duration-700 flex-none text-lg font-bold text-gray-800 dark:text-white">
         Card title
       </h3>
-      <p class="grow mt-2 flex text-gray-800 dark:text-gray-400 items-center content-center justify-center">
-        <input v-model="gameCode" type="text" class="max-w-[10rem] text-2xl font-mono py-3 px-4 block border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400">
+      <p class="transition-all duration-700 grow mt-2 flex text-gray-800 dark:text-gray-400 items-center content-center justify-center">
+        <input ref="inputField" v-model="gameCode" type="text" autofocus
+               class="transition-all duration-700 max-w-[10rem] text-2xl font-mono bg-gray-100 py-3 px-4 block border border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400">
       </p>
       <a class="flex-none mt-3 inline-flex mx-auto w-full items-center gap-2 mt-5 text-sm font-medium text-blue-500 hover:text-blue-700">
-        <button @click="startGame" type="button" class="py-[.688rem] px-4 mx-auto w-full inline-flex justify-center items-center gap-2 rounded-md border-2 border-gray-200 font-semibold text-blue-500 hover:text-white hover:bg-blue-500 hover:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:border-gray-700 dark:hover:border-blue-500">
+        <button @click="startGame" type="button"
+                class="py-[.688rem] px-4 mx-auto w-full inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">
           Start Playing
         </button>
       </a>
@@ -22,13 +24,18 @@
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue";
+import {onMounted, onRenderTracked, onRenderTriggered, ref} from "vue";
 import router from "@/router";
 import {useWebSocketStore} from "@/stores/websocket";
 import webSocketDataCode from "@/WebSocketDataCode";
 import {getBaseURL} from "@/getBaseURL";
 
 const gameCode = ref("")
+const inputField = ref<HTMLInputElement | null>(null)
+onMounted(() => {
+  inputField.value?.focus()
+})
+
 
 function startGame(){
 
