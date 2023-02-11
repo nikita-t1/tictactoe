3ee9531ea9c724684f85e3a177f67f289061deb9
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-02-11 18:14:34 +0100

    Remove Code from StatusCode Enum
    The @SerialName Annotation is actually enough

--- src/main/kotlin/dev/nikitatarasov/model/WebSocketDataCode.kt
+++ src/main/kotlin/dev/nikitatarasov/model/WebSocketDataCode.kt
@@ -9,24 +9,24 @@ class WebSocketDataCode(
     val msg: String = ""
 ) {
 
-
     companion object {
         @Serializable
-        enum class StatusCode(code: Int) {
-            @SerialName("4000") STATUS_OK(4000),
-            @SerialName("4001") SECOND_PLAYER_CONNECTED(4001),
-            @SerialName("4002") MOVE_ACCEPTED(4002),
-            @SerialName("4003") YOUR_MOVE(4003),
-            @SerialName("4004") OPPONENT_MOVE(4004),
-            @SerialName("4005") GAME_BOARD(4005),
-            @SerialName("4008") YOU_WON(4008),
-            @SerialName("4009") OPPONENT_WON(4009),
-            @SerialName("4010") NOT_YOUR_TURN(4010),
-            @SerialName("4011") MOVE_INVALID(4011),
-            @SerialName("4301") NO_SECOND_PLAYER_YET(4301),
-            @SerialName("4501") GAME_CODE_TIMEOUT_REACHED(4501),
-            @SerialName("4502") GAME_ALREADY_HAS_TWO_PLAYERS(4502),
-            @SerialName("4503") NO_GAMECODE(4503)
+        enum class StatusCode {
+            @SerialName("4000") STATUS_OK,
+            @SerialName("4001") SECOND_PLAYER_CONNECTED,
+            @SerialName("4002") MOVE_ACCEPTED,
+            @SerialName("4003") YOUR_MOVE,
+            @SerialName("4004") OPPONENT_MOVE,
+            @SerialName("4005") GAME_BOARD,
+            @SerialName("4008") YOU_WON,
+            @SerialName("4009") OPPONENT_WON,
+            @SerialName("4010") NOT_YOUR_TURN,
+            @SerialName("4011") MOVE_INVALID,
+            @SerialName("4301") NO_SECOND_PLAYER_YET,
+            @SerialName("4501") GAME_CODE_TIMEOUT_REACHED,
+            @SerialName("4502") GAME_ALREADY_HAS_TWO_PLAYERS,
+            @SerialName("4503") NO_GAMECODE,
+            @SerialName("4999") GENERIC_EXCEPTION
         }
     }
 }
