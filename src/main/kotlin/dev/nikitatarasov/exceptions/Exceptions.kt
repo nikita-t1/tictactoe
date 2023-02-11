999a77b3ff9707ecf03b55ac8374f84ec6b95aca
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-02-11 18:15:30 +0100

    Create own WebSocketExceptions

---
+++ src/main/kotlin/dev/nikitatarasov/exceptions/Exceptions.kt
@@ -0,0 +1,25 @@
+package dev.nikitatarasov.exceptions
+
+import dev.nikitatarasov.model.WebSocketDataCode.Companion.StatusCode as StatusCode
+
+
+class NoGameCodeException(
+    message: String? = "No GameCode was provided as Query Parameter, but one is required",
+    webSocketDataCode: StatusCode = StatusCode.NO_GAMECODE
+) : WebSocketException(message, webSocketDataCode)
+
+class TimeoutSecondPlayerException(
+    message: String? = "Timeout was reached before the second Player joined the Game",
+    webSocketDataCode: StatusCode = StatusCode.GAME_CODE_TIMEOUT_REACHED
+) : WebSocketException(message, webSocketDataCode)
+
+class GameLobbyIsFullException(
+    message: String? = "The Game with this Code already has 2 Players",
+    webSocketDataCode: StatusCode = StatusCode.GAME_ALREADY_HAS_TWO_PLAYERS
+) : WebSocketException(message)
+
+open class WebSocketException(
+    message: String? = null,
+    val webSocketDataCode: StatusCode = StatusCode.GENERIC_EXCEPTION,
+    cause: Throwable? = null
+) : Exception(message, cause)
