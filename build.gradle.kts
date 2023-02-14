2ccd3a400c5817cb81cac3bf0a6f3d9d1098b23f
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-02-14 20:44:53 +0100

    Add 'kotlinx-datetime' to build.gradle.kts
    A multiplatform Kotlin library for working with date and time.

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-cors-jvm:2.2.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
