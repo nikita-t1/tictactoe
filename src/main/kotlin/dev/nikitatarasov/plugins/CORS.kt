7dc23f6f0a4d0c8617542429341c4bb5586d313f
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-02-26 11:42:52 +0100

    Update CORS Policy

--- src/main/kotlin/dev/nikitatarasov/plugins/CORS.kt
+++ src/main/kotlin/dev/nikitatarasov/plugins/CORS.kt
@@ -1,18 +1,23 @@
 package dev.nikitatarasov.plugins
 
-import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.plugins.cors.routing.*
+import io.ktor.http.HttpMethod
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.cors.routing.CORS
 
 fun Application.configureCORS() {
-    install(CORS) {
-        allowMethod(HttpMethod.Options)
-        allowMethod(HttpMethod.Put)
-        allowMethod(HttpMethod.Delete)
-        allowMethod(HttpMethod.Patch)
-        allowMethod(HttpMethod.Post)
-        allowHeader(HttpHeaders.Authorization)
-        allowHeader("MyCustomHeader")
-        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    if (isDevMode()) {
+        install(CORS) {
+            allowMethod(HttpMethod.Post)
+
+            // default vite dev server port
+            allowHost("127.0.0.1:5173")
+            allowHost("localhost:5173")
+
+            // default vite preview server port
+            allowHost("127.0.0.1:4173")
+            allowHost("localhost:4173")
+        }
     }
+
 }
