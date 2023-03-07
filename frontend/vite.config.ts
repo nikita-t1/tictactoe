4686503c98dc59bd05189c5183742326010c5541
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-03-07 19:22:24 +0100

    Install 'vue-i18n' + 'unplugin-vue-i18n'
    Took 30 minutes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,15 @@ import {fileURLToPath, URL} from 'node:url'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Icons from 'unplugin-icons/vite'
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), Icons({ compiler: 'vue3'}) ],
+    plugins: [
+        vue(),
+        Icons({compiler: 'vue3'}),
+        VueI18nPlugin({ /* options */}),
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
