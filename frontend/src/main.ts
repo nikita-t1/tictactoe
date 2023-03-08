88937b5cc7d154b74639a1abeb02edd6b3c397a6
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-03-08 22:15:24 +0100

    Initialize i18n

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
 import {computed, createApp} from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
 import router from './router'
@@ -8,10 +9,22 @@ import './assets/main.css'
 import "preline"
 import {useFavicon, usePreferredDark} from "@vueuse/core";
 
+import en from './locales/en.json'
+import de from './locales/de-DE.json'
+
+const i18n = createI18n({
+    locale: 'en', // set locale
+    //fallbackLocale: 'en', // set fallback locale
+    messages: {en, de}, // set locale messages
+    // If you need to specify other options, you can set other options
+    // ...
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
 
