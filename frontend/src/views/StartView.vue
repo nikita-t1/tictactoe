a867cd2d5c3304aa385cb276c6f2b5ff70ac09c2
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-08-11 20:57:08 +0200

    design: remove start card border

--- frontend/src/views/StartView.vue
+++ frontend/src/views/StartView.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="flex flex-col max-w-4xl content-center self-center items-center justify-center mx-auto space-x-0 space-y-6 h-screen">
 
-      <div class="flex flex-col bg-gray-50 dark:bg-slate-900 border shadow-sm rounded-xl dark:border-gray-700 dark:shadow-slate-700/[.7]">
-          <div class=" border-b rounded-t-xl py-3 px-4 md:py-4 md:px-5 dark:border-gray-700">
-              <p class="mt-1 text-md text-gray-800 dark:text-white">
+      <div class="flex flex-col bg-gray-50 dark:bg-slate-900 rounded-xl dark:border-gray-700 dark:shadow-slate-700/[.7]">
+          <div class="rounded-t-xl py-3 px-4 md:py-4 md:px-5 dark:border-gray-700">
+              <p class="mt-1 text-md font-bold text-gray-800 dark:text-white">
                   Singleplayer
               </p>
           </div>
           <div class="p-4 md:p-5 flex flex-col md:flex-row max-w-4xl content-center self-center items-center justify-center mx-auto space-x-0 md:space-x-8 space-y-6 md:space-y-0">
 
               <div class="min-h-[14rem] transition-all duration-700 flex flex-col w-96 md:w-[50rem] bg-white border shadow-sm rounded-xl p-4 md:p-5 dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-                  <h3 class="transition-all duration-700 text-md font-bold text-gray-800 dark:text-white">
+                  <h3 class="transition-all duration-700 text-md font-semibold text-gray-800 dark:text-white">
                       {{t("play_vs_computer")}}
                   </h3>
                   <p class="grow transition-all duration-700 mt-6 text-gray-800 dark:text-gray-400">
@@ -26,15 +26,15 @@
           </div>
       </div>
 
-      <div class="flex flex-col bg-gray-50 dark:bg-slate-900 border shadow-sm rounded-xl dark:border-gray-700 dark:shadow-slate-700/[.7]">
-          <div class=" border-b rounded-t-xl py-3 px-4 md:py-4 md:px-5 dark:border-gray-700">
-              <p class="mt-1 text-md text-gray-800 dark:text-white">
+      <div class="flex flex-col bg-gray-50 dark:bg-slate-900 rounded-xl dark:border-gray-700 dark:shadow-slate-700/[.7]">
+          <div class="rounded-t-xl py-3 px-4 md:py-4 md:px-5 dark:border-gray-700">
+              <p class="mt-1 text-md font-bold text-gray-800 dark:text-white">
                   Multiplayer
               </p>
           </div>
           <div class="p-4 md:p-5 flex flex-col md:flex-row max-w-4xl content-center self-center items-center justify-center mx-auto space-x-0 md:space-x-8 space-y-6 md:space-y-0">
               <div class="min-h-[14rem] transition-all duration-700 flex flex-col w-96 bg-white border shadow-sm rounded-xl p-4 md:p-5 dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-                  <h3 class="transition-all duration-700 text-md font-bold text-gray-800 dark:text-white">
+                  <h3 class="transition-all duration-700 text-md font-semibold text-gray-800 dark:text-white">
                       {{t("player")}} 1
                   </h3>
                   <p class=" grow transition-all duration-700 mt-6 text-gray-800 dark:text-gray-400">
@@ -48,7 +48,7 @@
               </div>
 
               <div class="min-h-[14rem] transition-all duration-700 flex flex-col w-96 bg-white border shadow-sm rounded-xl p-4 md:p-5 dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-                  <h3 class="transition-all duration-700 text-md font-bold text-gray-800 dark:text-white">
+                  <h3 class="transition-all duration-700 text-md font-semibold text-gray-800 dark:text-white">
                       {{t("player")}} 2
                   </h3>
                   <p class="grow transition-all duration-700 mt-6 text-gray-800 dark:text-gray-400">
