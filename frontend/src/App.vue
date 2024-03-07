9d7061b0debaef64051e5ef919724ad9b46dc7c8
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2024-03-07 20:19:47 +0100

    fix: RouterView centered again

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,10 +5,10 @@ import NavBar from "@/components/NavBar.vue";
 </script>
 
 <template>
-  <div class="transition-all duration-700 bg-gray-50 dark:bg-slate-900 h-screen">
+  <div class="transition-all duration-700 bg-gray-50 dark:bg-slate-900 h-screen flex flex-col">
     <NavBar />
     <ThemeToggle />
-    <RouterView />
+    <RouterView class="grow"/>
   </div>
 </template>
 
