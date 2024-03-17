08687f5da0fd443c58518f0d042ded06e69759f9
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2024-03-17 20:59:10 +0100

    test: implement tests for ComputerBrainHelper and GameBoardHelper
    This commit introduces tests for the ComputerBrainHelper and GameBoardHelper. The tests check for various game scenarios such as identifying a winning move or a draw, and verifying the computer’s ability to block a win from the player, or make a random move when necessary.

---
+++ frontend/tests/helper/GameBoardHelper.test.ts
@@ -0,0 +1,51 @@
+import { test } from "vitest"
+import { EMPTY_BOARD_FIELD, MOVE_BY_PLAYER, MOVE_BY_COMPUTER, winConditions, checkDraw, checkWinner } from "../../src/helper/GameBoardHelper";
+
+test('checking winConditions constant', ({ expect }) => {
+    expect(winConditions.length).toBe(8) // As 8 possible win combinations for Tic Tac Toe
+})
+
+test('checkDraw correctly identifies draw', ({ expect }) => {
+    const fullBoard = [
+        MOVE_BY_PLAYER, MOVE_BY_COMPUTER, MOVE_BY_PLAYER,
+        MOVE_BY_COMPUTER, MOVE_BY_PLAYER, MOVE_BY_COMPUTER,
+        MOVE_BY_PLAYER, MOVE_BY_COMPUTER, MOVE_BY_PLAYER
+    ]
+    expect(checkDraw(fullBoard)).toBe(true)
+})
+
+test('checkDraw correctly identifies not a draw', ({ expect }) => {
+    const notFullBoard = [
+        MOVE_BY_PLAYER, MOVE_BY_COMPUTER, MOVE_BY_PLAYER,
+        MOVE_BY_COMPUTER, MOVE_BY_PLAYER, EMPTY_BOARD_FIELD,
+        MOVE_BY_PLAYER, MOVE_BY_COMPUTER, MOVE_BY_PLAYER
+    ]
+    expect(checkDraw(notFullBoard)).toBe(false)
+})
+
+test('checkWinner correctly identifies player as winner', ({ expect }) => {
+    const winningPlayerBoard = [
+        MOVE_BY_PLAYER, MOVE_BY_PLAYER, MOVE_BY_PLAYER,
+        MOVE_BY_COMPUTER, EMPTY_BOARD_FIELD, MOVE_BY_COMPUTER,
+        EMPTY_BOARD_FIELD, MOVE_BY_COMPUTER, EMPTY_BOARD_FIELD
+    ]
+    expect(checkWinner(winningPlayerBoard)).toBe(MOVE_BY_PLAYER)
+})
+
+test('checkWinner correctly identifies opponent as winner', ({ expect }) => {
+    const winningComputerBoard = [
+        MOVE_BY_COMPUTER, MOVE_BY_PLAYER, MOVE_BY_PLAYER,
+        MOVE_BY_COMPUTER, MOVE_BY_PLAYER, MOVE_BY_PLAYER,
+        MOVE_BY_COMPUTER, EMPTY_BOARD_FIELD, EMPTY_BOARD_FIELD
+    ]
+    expect(checkWinner(winningComputerBoard)).toBe(MOVE_BY_COMPUTER)
+})
+
+test('checkWinner correctly identifies a draw', ({ expect }) => {
+    const drawBoard = [
+        MOVE_BY_PLAYER, MOVE_BY_COMPUTER, MOVE_BY_PLAYER,
+        MOVE_BY_COMPUTER, MOVE_BY_COMPUTER, MOVE_BY_PLAYER,
+        MOVE_BY_PLAYER, MOVE_BY_PLAYER, MOVE_BY_COMPUTER
+    ]
+    expect(checkWinner(drawBoard)).toBe(EMPTY_BOARD_FIELD)
+})
