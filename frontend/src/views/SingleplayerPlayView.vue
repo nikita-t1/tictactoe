dfb1e51525f93b63655c88a4f762368f39b108f5
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2024-03-17 21:01:52 +0100

    test: implement tests for 'SingleplayerPlayView' component

--- frontend/src/views/SingleplayerPlayView.vue
+++ frontend/src/views/SingleplayerPlayView.vue
@@ -17,7 +17,7 @@
              class="transition-all duration-700 flex flex-col md:flex-row max-w-xl content-center self-center items-center justify-center mx-auto space-x-2">
             <div
                 class="flex-none mt-10 mx-auto inline-flex items-center gap-2 text-sm font-medium text-blue-500 hover:text-blue-700">
-                <button type="button" @click="gameStore.replay"
+                <button type="button" @click="gameStore.replay" id="replay_btn"
                         class="transition-all duration-700 py-[.688rem] px-4 mx-auto w-48 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-600 hover:bg-blue-700 text-white text-sm dark:focus:ring-offset-gray-800">
                     {{ t("replay") }}
                 </button>
---
+++ frontend/tests/views/SingleplayerPlayView.test.ts
@@ -0,0 +1,56 @@
+import {mount} from '@vue/test-utils'
+import SingleplayerPlayView from "../../src/views/SingleplayerPlayView.vue";
+import {useGameStore} from "../../src/stores/useGameStore";
+import {describe, test, vi} from 'vitest'
+import {storeToRefs} from "pinia";
+import {options} from "../globalMocks";
+import {WebSocketCodes} from "../../src/StatusCodes";
+import {EMPTY_BOARD_FIELD, MOVE_BY_PLAYER} from "../../src/helper/GameBoardHelper";
+
+
+describe('SingleplayerPlayView', () => {
+
+    test('restart game on mount', ({expect}) => {
+        const gameStore = useGameStore()
+        mount(SingleplayerPlayView, options)
+        expect(gameStore.replay).toHaveBeenCalledTimes(1)
+    })
+
+    test('renders correctly based on props', ({expect}) => {
+        const gameStore = useGameStore()
+        const {currentStatusCode, hasGameEnded, awaitingMoveBy, gameBoard} = storeToRefs(gameStore)
+
+        currentStatusCode.value = WebSocketCodes.STATUS_OK
+        hasGameEnded.value = true
+        awaitingMoveBy.value = MOVE_BY_PLAYER
+        gameBoard.value = Array(9).fill(EMPTY_BOARD_FIELD)
+
+        let wrapper = mount(SingleplayerPlayView, options)
+
+        // find the 'replay' and 'home' buttons
+        expect(wrapper.findAll('button').length).toBe(2)
+
+        hasGameEnded.value = false
+        wrapper = mount(SingleplayerPlayView, options)
+        // 'replay' and 'home' buttons should not be visible when game is ongoing
+        expect(wrapper.findAll('button').length).toBe(0)
+    })
+
+    test('run functions on button clicks', ({expect}) => {
+        const gameStore = useGameStore()
+        const replaySpy = vi.spyOn(gameStore, 'replay')
+        const {currentStatusCode, hasGameEnded, awaitingMoveBy, gameBoard} = storeToRefs(gameStore)
+
+        hasGameEnded.value = true
+        currentStatusCode.value = WebSocketCodes.STATUS_OK
+
+        const wrapper = mount(SingleplayerPlayView, options)
+        expect(gameStore.replay).toHaveBeenCalledTimes(1)
+
+        expect(wrapper.find('#replay_btn').exists()).toBe(true)
+        wrapper.find('#replay_btn').trigger('click')
+        // replay should be called twice, once on mount and once on button click
+        expect(replaySpy).toHaveBeenCalledTimes(2)
+    })
+
+})
