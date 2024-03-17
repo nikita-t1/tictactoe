5292260c02b3a6aa44b75868dad53294d95d87fe
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2024-03-17 20:57:43 +0100

    test: refactor and extract global mocks
    Arrange import statements in CreateGameCode.test.ts in alphabetical order. Remove unused i18n and createPinia imports and add the recently created 'globalMocks.ts' file which includes these and other common testing utilities such as useI18n and options. This aims to make individual test files cleaner and more maintainable.

--- frontend/tests/components/CreateGameCode.test.ts
+++ frontend/tests/components/CreateGameCode.test.ts
@@ -1,31 +1,14 @@
-import { mount } from '@vue/test-utils'
-import {beforeEach, afterEach, describe, it, expect, vi, beforeAll, afterAll} from 'vitest'
+import {mount} from '@vue/test-utils'
+import {afterAll, afterEach, beforeAll, beforeEach, describe, expect, it} from 'vitest'
 import axios from 'axios'
 import AxiosMockAdapter from 'axios-mock-adapter'
 import CreateGameCode from "../../src/components/CreateGameCode.vue";
-import i18n from "../../src/plugins/i18n";
-import {createPinia} from "pinia";
-import {backendServerMock, WebSocketMock} from "../globalMocks";
+import {backendServerMock, options} from "../globalMocks";
 import {Server} from "mock-socket";
 
 let mock: InstanceType<typeof AxiosMockAdapter>
 let backendServer: Server
 
-
-export const useI18n = () => {
-    return {
-        t: (key: string) => key,
-    };
-};
-
-const options = {
-    global: {
-        plugins: [i18n, createPinia()],
-        mocks: {
-            $t: useI18n().t,
-        },
-    },
-};
 
 describe('CreateGameCode', () => {
     beforeAll(() => {
--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     ],
     // @ts-ignore
     test: {
-        globalThis: true,
+        // globals: true,
         environment: 'happy-dom'
         // ... Specify options here.
     },
