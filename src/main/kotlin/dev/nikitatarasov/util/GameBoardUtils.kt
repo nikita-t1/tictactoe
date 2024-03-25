245b5957bbe25568ff862f359adfc6d74cc4419f
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2024-03-25 22:28:12 +0100

    fix: modify GameBoardUtils to ignore unset player symbol
    Adjusted the conditional statements in the GameBoardUtils class to include checks for unset player symbols before assigning the winning symbol. These added conditions prevent prematurely declaring a winner when player symbols on the gameboard have not been set, ensuring accurate game results.

--- src/main/kotlin/dev/nikitatarasov/util/GameBoardUtils.kt
+++ src/main/kotlin/dev/nikitatarasov/util/GameBoardUtils.kt
@@ -39,42 +39,58 @@ object GameBoardUtils {
             // |x|x|x|
             // | | | |
             // | | | |
-            if (gamefield[0][0].equalsAll(gamefield[0][1], gamefield[0][2])) winningSymbol = gamefield[0][0]
+            if (gamefield[0][0].equalsAll(gamefield[0][1], gamefield[0][2]) && gamefield[0][0] != PlayerSymbol.UNSET) {
+                winningSymbol = gamefield[0][0]
+            }
 
             // | | | |
             // |x|x|x|
             // | | | |
-            else if (gamefield[1][0].equalsAll(gamefield[1][1], gamefield[1][2])) winningSymbol = gamefield[1][0]
+            else if (gamefield[1][0].equalsAll(gamefield[1][1], gamefield[1][2]) && gamefield[1][0] != PlayerSymbol.UNSET) {
+                winningSymbol = gamefield[1][0]
+            }
 
             // | | | |
             // | | | |
             // |x|x|x|
-            else if (gamefield[2][0].equalsAll(gamefield[2][1], gamefield[2][2])) winningSymbol = gamefield[2][0]
+            else if (gamefield[2][0].equalsAll(gamefield[2][1], gamefield[2][2]) && gamefield[2][0] != PlayerSymbol.UNSET) {
+                winningSymbol = gamefield[2][0]
+            }
 
             // |x| | |
             // |x| | |
             // |x| | |
-            else if (gamefield[0][0].equalsAll(gamefield[1][0], gamefield[2][0])) winningSymbol = gamefield[0][0]
+            else if (gamefield[0][0].equalsAll(gamefield[1][0], gamefield[2][0]) && gamefield[0][0] != PlayerSymbol.UNSET) {
+                winningSymbol = gamefield[0][0]
+            }
 
             // | |x| |
             // | |x| |
             // | |x| |
-            else if (gamefield[0][1].equalsAll(gamefield[1][1], gamefield[2][1])) winningSymbol = gamefield[0][1]
+            else if (gamefield[0][1].equalsAll(gamefield[1][1], gamefield[2][1]) && gamefield[0][1] != PlayerSymbol.UNSET) {
+                winningSymbol = gamefield[0][1]
+            }
 
             // | | |x|
             // | | |x|
             // | | |x|
-            else if (gamefield[0][2].equalsAll(gamefield[1][2], gamefield[2][2])) winningSymbol = gamefield[0][2]
+            else if (gamefield[0][2].equalsAll(gamefield[1][2], gamefield[2][2]) && gamefield[0][2] != PlayerSymbol.UNSET) {
+                winningSymbol = gamefield[0][2]
+            }
 
             // |x| | |
             // | |x| |
             // | | |x|
-            else if (gamefield[0][0].equalsAll(gamefield[1][1], gamefield[2][2])) winningSymbol = gamefield[0][0]
+            else if (gamefield[0][0].equalsAll(gamefield[1][1], gamefield[2][2]) && gamefield[0][0] != PlayerSymbol.UNSET) {
+                winningSymbol = gamefield[0][0]
+            }
 
             // | | |x|
             // | |x| |
             // |x| | |
-            else if (gamefield[0][2].equalsAll(gamefield[1][1], gamefield[2][0])) winningSymbol = gamefield[0][2]
+            else if (gamefield[0][2].equalsAll(gamefield[1][1], gamefield[2][0]) && gamefield[0][2] != PlayerSymbol.UNSET) {
+                winningSymbol = gamefield[0][2]
+            }
 
             logger.info("symbol " + winningSymbol?.fieldToSymbol())
             return winningSymbol
