407d32c03bc599b06a8b11f33a3440669e8fcb73
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2024-03-27 20:38:56 +0100

    refactor status codes in server and frontend code
    Status codes in the code have been refined and rearranged. This includes renumbering codes, removing redundant ones, and adding new statuses to effectively manage game states. The codes are further categorised based on their functionalities, adding clarity to the code readability and maintenance.

--- src/main/kotlin/dev/nikitatarasov/model/StatusCode.kt
+++ src/main/kotlin/dev/nikitatarasov/model/StatusCode.kt
@@ -6,37 +6,41 @@ import kotlinx.serialization.Serializable
 @Suppress("MagicNumber")
 @Serializable
 enum class StatusCode(val code: Int) {
+
+    /**
+     * These codes represent the initial state of the game.
+    */
     @SerialName("4000") STATUS_OK(4000),
-    @SerialName("4002") GAME_STARTED(4002),
-//    @SerialName("4001") GAME_BOARD(4001),
+    @SerialName("4001") GAME_STARTED(4001),
+    @SerialName("4002") BOTH_PLAYERS_CONNECTED(4002),
 
-    @SerialName("4100") BOTH_PLAYERS_CONNECTED(4100),
-//    @SerialName("4101") FIRST_PLAYER_CONNECTED(4101),
-//    @SerialName("4102") SECOND_PLAYER_CONNECTED(4102),
-    @SerialName("4103") YOUR_MOVE(4103),
-//    @SerialName("4104") OPPONENT_MOVE(4104),
-//    @SerialName("4105") REQUEST_REMATCH(4105),
-    @SerialName("4106") REMATCH_REQUESTED(4106),
-//    @SerialName("4107") ACCEPT_REMATCH(4107),
-    @SerialName("4108") REMATCH_ACCEPTED(4108),
+    /**
+     * These codes are related to the gameplay and determining the game's end
+    */
+    @SerialName("4200") YOUR_MOVE(4200),
+    @SerialName("4201") MOVE_ACCEPTED(4201),
+    @SerialName("4203") GAME_ENDED(4204), // winner will be evaluated by the client
 
-    @SerialName("4200") MOVE_ACCEPTED(4200),
-//    @SerialName("4201") YOU_WON(4201),
-//    @SerialName("4202") OPPONENT_WON(4202),
-//    @SerialName("4203") GAME_ENDED_IN_DRAW(4203),
-    @SerialName("4204") GAME_ENDED(4204), // winner will be evaluated by the client
+    /**
+     * These codes handle the process of requesting and accepting a rematch
+    */
+    @SerialName("4300") REMATCH_REQUESTED(4300),
+    @SerialName("4301") REMATCH_ACCEPTED(4301),
 
-    @SerialName("4300") NO_OPPONENT_YET(4300),
-//    @SerialName("4301") NO_FIRST_PLAYER_YET(4301),
-//    @SerialName("4302") NO_SECOND_PLAYER_YET(4302),
-    @SerialName("4303") OPPONENT_DISCONNECTED(4303),
+    /**
+     * These codes are used to address issues with the opponent or the game itself
+    */
+    @SerialName("4400") NO_OPPONENT_YET(4400),
+    @SerialName("4401") OPPONENT_DISCONNECTED(4401),
+    @SerialName("4411") NOT_YOUR_TURN(4411),
+    @SerialName("4412") INVALID_MOVE(4412),
+    @SerialName("4413") GAME_ALREADY_HAS_TWO_PLAYERS(4413),
+    @SerialName("4514") NO_GAMECODE_IN_QUERY(4514),
+    @SerialName("4515") NO_GAME_WITH_THIS_CODE_FOUND(4515),
 
-    @SerialName("4401") NOT_YOUR_TURN(4401),
-    @SerialName("4402") INVALID_MOVE(4402),
-    @SerialName("4403") GAME_ALREADY_HAS_TWO_PLAYERS(4403),
-    @SerialName("4504") NO_GAMECODE_IN_QUERY(4504),
-    @SerialName("4505") NO_GAME_WITH_THIS_CODE_FOUND(4505),
-
+    /**
+     * These codes manage unique gamecode scenarios and general exceptions
+    */
     @SerialName("4501") GAME_CODE_TIMEOUT_REACHED(4501),
     @SerialName("4599") GENERIC_EXCEPTION(4599);
 }
